3701bce5927e43dc2b1a8d05e8ad8292d2a1b6b4
TFD10507 <ydolemlinn@gmail.com>
2022-04-16 14:40:51 +0800

    [feat] 新增拿取商品id的php、修改商品頁的vue id

--- php/getproductInfo.php
+++ php/getproductInfo.php
@@ -1,16 +1,14 @@
 <?php
-    require_once("connect.php");
-    $sql = "select * from product where psn=:psn";
-    $product = $pdo->prepare($sql);
-    $product->bindValue(":id", $_GET['id']);
-    $product->execute();
-    if ($product->rowCount() == 0) { //找不到
-        //傳回空的JSON字串
-        echo "{}";
-    } else { //找得到
-        //取回一筆資料
-        $productRow = $product->fetchAll(PDO::FETCH_ASSOC);
-        //送出json字串
-        echo json_encode($productRow);
-    }
+include("connection.php");
+
+$id = $_POST["id"];
+
+$sql = "select * from product where id=${id}";
+$product = $pdo->prepare($sql);
+// $product->bindValue(":id", $_POST['id']);
+$product->execute();
+$productRow = $product->fetchAll();
+//送出json字串
+echo json_encode($productRow);
+
 ?>
