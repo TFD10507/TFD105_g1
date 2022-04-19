da9caf883e31958434996f70309c66427bfc8956
JianTing321 <jason717011@gmail.com>
2022-04-19 17:33:38 +0800

    [feat]修正php檔案，頁面修改。

--- php/products_kind.php
+++ php/products_kind.php
@@ -15,7 +15,7 @@
 
    $kind = $_POST['kind'];
    // 把所有在資料庫的商品都選擇出來
-   $sql = "SELECT * FROM product where kind = '$kind'";
+   $sql = "SELECT * FROM product where kind_id = '$kind'";
    
    $statement = $pdo->query($sql);
    $data = $statement->fetchAll();
