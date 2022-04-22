649c03530f89f4c61e7fb2ce31e5c2329980e85b
efron15 <fundate0115@gmail.com>
2022-04-22 14:07:02 +0800

    [ feat ] 新增更新訂單.客製化訂單PHP

---
+++ php/Update_customization.php
@@ -0,0 +1,25 @@
+<?php
+// 這是類是串聯別頁PHP
+require("./Connection.php");
+//---------------------------------------------------
+$customization = json_decode(file_get_contents("php://input"));
+echo json_encode($customization);
+
+// // SQL 語法
+$sql = "update customization
+              set 
+                  status=:status
+              where
+                  id=:id;";
+
+//使用這個包裝才可以讓PHP,使用自己定義的 Value
+$statement = $link->prepare($sql);
+
+// 自定義 Value區
+$statement->bindValue(":id", $customization->UPid);
+$statement->bindValue(":status", $customization->UPstatus);
+
+// 執行
+$statement->execute();
+
+?>
