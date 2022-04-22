8ddd362daf68e375d42c6a2bd725741ceb189422
heng-ning <maggie310254@gmail.com>
2022-04-22 10:37:00 +0800

    [feat] 新增php 印出 會員id

--- php/login_new.php
+++ php/login_new.php
@@ -18,12 +18,16 @@ $statement->bindValue(":password", $member["login_password"]);
 
 // 執行
 $statement->execute();
-
+$data = $statement->fetchAll();
 $resultCount = $statement->rowCount();
 
 if ($resultCount > 0) {
+   $respBody["id"] = $data[0]["id"];
+   $respBody["account"] = $data[0]["account"];
+   // $respBody["password"] = $data[0]["password"];
    $respBody["successful"] = true;
    // $respBody["message"] = "s";
+   // echo json_encode($respBody);
 } else {
    $respBody["successful"] = false;
    // $respBody["message"] = "f";
