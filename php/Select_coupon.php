8efe21c1fbfe8f5b329d23a41bd2e82b8a186deb
efron15 <fundate0115@gmail.com>
2022-04-22 15:57:03 +0800

    [ feat ] 移除註解

--- php/Select_coupon.php
+++ php/Select_coupon.php
@@ -18,9 +18,6 @@
               $statement = $link->query($sql);
        }
 
-       //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
-       // $statement = $link->query($sql);
-
        //抓出全部且依照順序封裝成一個二維陣列
        $data = $statement->fetchAll(PDO::FETCH_ASSOC);
 
