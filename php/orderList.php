9644740cfa4493d5361ec2cd0418b386925cd842
yingchuw <yingchu.w@gmail.com>
2022-04-23 15:37:46 +0800

    [feat] 修正$SQL語法

--- php/orderList.php
+++ php/orderList.php
@@ -26,7 +26,7 @@ $SQL = " SELECT
             on ol.product_id = p.id
          where m.id = 1
          group by
-            o.id";
+         o.id";
 
 //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
 $statement = $link->query($SQL);
