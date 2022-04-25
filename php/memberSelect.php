041a6e2c2d3f949b18ca79fa2f4f99990127729b
yingchuw <yingchu.w@gmail.com>
2022-04-25 10:40:16 +0800

    [feat] passwordUpdate.php 刪除不用的code, member.html 修改密碼JS修改順序

--- php/memberSelect.php
+++ php/memberSelect.php
@@ -5,44 +5,58 @@
 
        //---------------------------------------------------
        $member = json_decode(file_get_contents("php://input"));
- 
-       if(isset($member)){    //如果html有傳值,先找值對應的ID
-              $sql = "SELECT * FROM G1.member WHERE id = 3";
-              // $statement = $link->prepare($sql);
-              // $statement->bindValue(":id", $member->memberid);
+       
+       // print_r($member);
+       echo json_encode($member);
+       // echo 'fff';
+       exit;
+       
+       $sql = "SELECT * FROM member WHERE id = :id";
+              $statement = $link->prepare($sql);
+              $statement->bindValue(":id", $member->id);
               $statement->execute();
-       }else{ //沒有傳值,就改成搜索全部會員資料
-              //建立SQL語法   *改成那張表的搜尋全部資料
-              // $sql = "SELECT * FROM G1.member Order by id";
-              $sql = "SELECT * FROM G1.member WHERE id = 3";
 
-              //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
-              $statement = $link->query($sql);
-              // $statement = $pdo->query($sql);
-       }
-
-       //抓出全部且依照順序封裝成一個二維陣列
        $data = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-       //狀態數字改寫
-       foreach($data as $status => $value){
-              if($value['status'] == 0){
-                     $data[$status]['text'] = '正常';
-              }else if($value['status'] == 1){
-                     $data[$status]['text'] = '凍結';
-              }
+       // if(isset($member)){    //如果html有傳值,先找值對應的ID
+       //        $sql = "SELECT * FROM member WHERE id = :id";
+       //        $statement = $link->prepare($sql);
+       //        $statement->bindValue(":id", $member->id);
+       //        $statement->execute();
+       // }else{ //沒有傳值,就改成搜索全部會員資料
+       //        //建立SQL語法   *改成那張表的搜尋全部資料
+       //        // $sql = "SELECT * FROM G1.member Order by id";
+       //        $sql = "SELECT * FROM member WHERE id = :id";
 
-              if($value['gender'] == 0){
-                     $data[$status]['gender'] = '男性';
-              }else if($value['gender'] == 1){
-                     $data[$status]['gender'] = '女性';
-              }else if($value['gender'] == 2){
-                     $data[$status]['gender'] = '第三性';
-              }
-       };
+       //        //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
+       //        $statement = $link->prepare($sql);
+       //        // $statement->bindValue(":id", $member->id);
+       //        $statement->execute();
+       //        // $statement = $pdo->query($sql);
+       // }
+
+       //抓出全部且依照順序封裝成一個二維陣列
+       // $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+       // $data = $statement->fetchAll();
+
+       //狀態數字改寫
+       // foreach($data as $status => $value){
+       //        if($value['status'] == 0){
+       //               $data[$status]['text'] = '正常';
+       //        }else if($value['status'] == 1){
+       //               $data[$status]['text'] = '凍結';
+       //        }
+
+       //        if($value['gender'] == 0){
+       //               $data[$status]['gender'] = '男性';
+       //        }else if($value['gender'] == 1){
+       //               $data[$status]['gender'] = '女性';
+       //        }else if($value['gender'] == 2){
+       //               $data[$status]['gender'] = '第三性';
+       //        }
+       // };
 
        // 轉回去JSON檔案
        echo json_encode($data);
-
-
+       
 ?>
