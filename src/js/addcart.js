b27f2f1bc434c000b7c49e8b8e5d14bb45429977
TFD10507 <ydolemlinn@gmail.com>
2022-04-25 18:45:35 +0800

    Merge branch 'dev' into 77
    # Conflicts:
    #	src/js/addcart.js

--- src/js/addcart.js
+++ src/js/addcart.js
@@ -37,7 +37,7 @@ new Vue({
         this.id = urlParams.get('id');
     },
     methods: {
-        //加入購物車-存入sessionStorage
+        //加入購物車-存入sessionStorage        
         addcart() {
             loginMember("<strong>已加入購物車</strong>", 'success');
             let goods = {
@@ -121,7 +121,7 @@ new Vue({
                     sessionStorage.setItem("collect", JSON.stringify([storagedata]));
                 }
             }else{
-                loginMember('<strong>請先登入會員</strong>', 'error', '<button class="btn btn-warning m-3"><a href="/login.html" style="color: #fff">登入</a></button> ');
+                loginMember('<strong>請先登入會員</strong>', 'error', '<button class="btn btn-warning m-3"><a href="./login.html" style="color: #fff">登入</a></button> ');
             }
         },
         remove_love(num) {
