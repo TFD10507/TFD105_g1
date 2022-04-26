5ea39e2f8165420a7ade0519dfac646f2a160c57
TFD10507 <ydolemlinn@gmail.com>
2022-04-26 16:38:13 +0800

    [feat] 修改結帳的登入連結

--- src/js/cart.js
+++ src/js/cart.js
@@ -257,7 +257,7 @@ new Vue({
             sessionStorage.setItem("coupon",this.coupon.price);
           }
         } else {
-          loginMember('<strong>請先登入會員</strong>', 'error', '<button class="btn btn-warning m-3"><a href="/login.html" style="color: #fff">登入</a></button> ');
+          loginMember('<strong>請先登入會員</strong>', 'error', '<button class="btn btn-warning m-3"><a href="./login.html" style="color: #fff">登入</a></button> ');
         }
       } else {
         loginMember('<strong>購物車是空的唷！</strong>', 'warning');
